d1351303ce95e4627919145c0ffc4a3868df0383
fettuccine <fettuccine@gmail.com>
2016-09-09 21:32:49 +0900

    add rspec as test framework

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,15 @@ module RouteApi
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.autoload_paths += %W(#{config.root}/lib)
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: true,
+      request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
