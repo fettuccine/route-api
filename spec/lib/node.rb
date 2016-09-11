387f523aa1218c49f2d137e71700b6126a4c5b38
fettuccine <fettuccine@gmail.com>
2016-09-11 15:27:00 +0900

    remove node file

--- spec/lib/node.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Node do
-  describe Node.instance do
-    it 'should be same instance when called twice' do
-      expect(Node.instance).to eq(Node.instance)
-    end
-  end
-end
