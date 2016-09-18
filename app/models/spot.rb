3870f6f4273551a06277c3cb76354bddbba2b147
fettuccine <fettuccine@gmail.com>
2016-09-18 13:25:35 +0900

    add 1 test

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,6 +2,6 @@ class Spot
   include Neo4j::ActiveNode
   property :name, type: String
   property :desc, type: String
-  property :longitude, type: Float
-  property :latitude, type: Float
+  property :long, type: Float
+  property :lat, type: Float
 end
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe Spot do
+  before(:each) do
+    Spot.delete_all
+  end
   it 'should create' do
-    Spot.create(name: 'Route 246')
+    @spot = Spot.create(
+      name: 'Route 246',
+      desc: 'Loren Ipsum',
+      long: 180.0,
+      lat: 90.0
+    )
+    @spot.save
+    expect(Spot.all.size).to eq 1
   end
 end
