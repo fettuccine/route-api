8f500c0b1d535175edaf93b511a2e01701d55f05
fettuccine <fettuccine@gmail.com>
2016-09-18 17:29:23 +0900

    add testcase of post

--- spec/api/root_spec.rb
+++ spec/api/root_spec.rb
@@ -6,9 +6,25 @@ describe Root::API do
       get '/api/v1/routes'
       expect(response.status).to eq 200
     end
+  end
+  context "GET /api/v1/routes/1" do
     it "return routes of id" do
       get '/api/v1/routes/1'do
         expect(response.status).to eq 200
+      end
+    end
+  end
+  context "POST /api/v1/routes" do
+    it "create 1 new route" do
+      statuses = [{name: "test"}, {name: "test2"}]
+      post '/api/v1/routes', statuses.to_json, 'CONTENT_TYPE' => 'application/json' do
+        expect(response.status).to eq 201
+      end
+    end
+    it "create many new routes" do
+      statuses = {name: "test"}
+      post '/api/v1/routes', statuses.to_json, 'CONTENT_TYPE' => 'application/json' do
+        expect(response.body).to eq 201
       end
     end
   end
@@ -18,6 +34,8 @@ describe Root::API do
         expect(response.status).to eq 200
       end
     end
+  end
+  context "GET /api/v1/spots/1" do
     it "return spot of id" do
       get '/api/v1/spots/1' do
         expect(response.status).to eq 200
