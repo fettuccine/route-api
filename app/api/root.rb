b388f44025e630d1bf3b53aafb02f66af028d15e
fettuccine <fettuccine@gmail.com>
2016-09-18 13:51:38 +0900

    add rescue for when no record

--- app/api/root.rb
+++ app/api/root.rb
@@ -4,11 +4,28 @@ module Root
     format :json
     prefix :api
 
+    rescue_from Neo4j::ActiveNode::Labels::RecordNotFound do |e|
+        rack_response({ message: e.message, status: 404 }.to_json, 404)
+      end
+
     # http://localhost:3000/api/v1/routes/list
     resource :routes do
       desc 'Return all route.'
       get do
         {:a => "test"}
+      end
+    end
+    resource :spots do
+      desc 'Return all spot.'
+      get do
+        Spot.all
+      end
+      desc 'Return all spot.'
+      params do
+        requires :id, type: Integer, desc: 'Spot id.'
+      end
+      get ':id' do
+        Spot.find(params[:id])
       end
     end
   end
--- spec/api/root_spec.rb
+++ spec/api/root_spec.rb
@@ -7,4 +7,16 @@ describe Root::API do
       expect(response.status).to eq(200)
     end
   end
+  context "GET /api/v1/spots" do
+    it "return all spot" do
+      get '/api/v1/spots' do
+        expect(response.status).to eq 200
+      end
+    end
+    it "return spot of id" do
+      get '/api/v1/spots/1' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
 end
