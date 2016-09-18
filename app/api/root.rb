b6eb5a06e7916499b66192ed4796d299e7f443bd
fettuccine <fettuccine@gmail.com>
2016-09-18 13:52:57 +0900

    add 500 error handling

--- app/api/root.rb
+++ app/api/root.rb
@@ -5,8 +5,16 @@ module Root
     prefix :api
 
     rescue_from Neo4j::ActiveNode::Labels::RecordNotFound do |e|
-        rack_response({ message: e.message, status: 404 }.to_json, 404)
+      rack_response({ message: e.message, status: 404 }.to_json, 404)
+    end
+
+    rescue_from :all do |e|
+      if Rails.env.development?
+        raise e
+      else
+        error_response(message: "Internal server error", status: 500)
       end
+    end
 
     # http://localhost:3000/api/v1/routes/list
     resource :routes do
