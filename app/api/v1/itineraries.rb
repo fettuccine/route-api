9dec3ceeb7ce32096a8315462bc0f533009152f6
fettuccine <fettuccine@gmail.com>
2016-09-19 13:42:28 +0900

    add itineraries api

---
+++ app/api/v1/itineraries.rb
@@ -0,0 +1,26 @@
+module V1
+  class Itineraries < Grape::API
+    # http://localhost:3000/api/v1/routes/list
+    resource :itineraries do
+      desc 'Return all itinerary.'
+      get do
+        Itinerary.all
+      end
+      desc 'Return specified itinerary id.'
+      params do
+        requires :id, type: Integer, desc: 'Spot id.'
+      end
+      get ':id' do
+        Itinerary.find(params[:id])
+      end
+      desc 'Create a new itinerary.'
+      params do
+        requires :name, type: String
+      end
+      post do
+        @itinerary = Itinerary.create(name: params[:name])
+        @itinerary.save
+      end
+    end
+  end
+end
---
+++ spec/api/v1/itineraries_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe V1::Itineraries do
+  before(:each) do
+    Itinerary.delete_all
+  end
+  context "GET /api/v1/itineraries" do
+    it "return empty array" do
+      get '/api/v1/itineraries'
+      expect(response.status).to eq 200
+    end
+  end
+  context "GET /api/v1/itineraries/1" do
+    it "return itineraries of id" do
+      get '/api/v1/itineraries/1'do
+        expect(response.status).to eq 200
+      end
+    end
+  end
+  context "POST /api/v1/itineraries" do
+    it "create 1 new itinerary" do
+      statuses = [{name: "test"}, {name: "test2"}]
+      post '/api/v1/itineraries', statuses.to_json, 'CONTENT_TYPE' => 'application/json' do
+        expect(response.status).to eq 201
+      end
+    end
+    it "create many new itineraries" do
+      statuses = {name: "test"}
+      post '/api/v1/itineraries', statuses.to_json, 'CONTENT_TYPE' => 'application/json' do
+        expect(response.body).to eq 201
+      end
+      expect(Itinerary.all.size).to eq 1
+    end
+  end
+end
