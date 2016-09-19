00d0ff4b62ad4a13666ad43bb201e779f407c639
fettuccine <fettuccine@gmail.com>
2016-09-19 15:45:23 +0900

    refactoring

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -7,14 +7,14 @@ describe Route do
   it 'create record' do
     @spot1 = Spot.new(name: 'test1')
     @spot2 = Spot.new(name: 'test2')
-    rel1 = Route.create(from_node: @spot1, to_node: @spot2)
+    Route.create(from_node: @spot1, to_node: @spot2)
     expect(@spot1.spots.count).to eq 1
     expect(@spot2.spots.count).to eq 0
     #route should exist.
     @spot1.spots.each_rel do |rel|
       expect(rel.class).to eq Route
     end
-    retl2 = Route.create(from_node: @spot2, to_node: @spot1)
+    Route.create(from_node: @spot2, to_node: @spot1)
     expect(@spot1.spots.count).to eq 1
     expect(@spot2.spots.count).to eq 1
     #route should exist.
