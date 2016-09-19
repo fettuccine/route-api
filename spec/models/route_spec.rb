c0f990e27e30bbb2a4a908068699d7ebb72c39bb
fettuccine <fettuccine@gmail.com>
2016-09-19 15:39:05 +0900

    fix testcase for route.rb

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -2,12 +2,24 @@ require 'spec_helper'
 
 describe Route do
   before(:each) do
-    Route.delete_all
+    Spot.delete_all
   end
   it 'create record' do
-    @route = Route.create
-    @route.name = 'test'
-    @route.save
-    expect(Route.all.size).to eq 1
+    @spot1 = Spot.new(name: 'test1')
+    @spot2 = Spot.new(name: 'test2')
+    rel1 = Route.create(from_node: @spot1, to_node: @spot2)
+    expect(@spot1.spots.count).to eq 1
+    expect(@spot2.spots.count).to eq 0
+    #route should exist.
+    @spot1.spots.each_rel do |rel|
+      expect(rel.class).to eq Route
+    end
+    retl2 = Route.create(from_node: @spot2, to_node: @spot1)
+    expect(@spot1.spots.count).to eq 1
+    expect(@spot2.spots.count).to eq 1
+    #route should exist.
+    @spot2.spots.each_rel do |rel|
+      expect(rel.class).to eq Route
+    end
   end
 end
